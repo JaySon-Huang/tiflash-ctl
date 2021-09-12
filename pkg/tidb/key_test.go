492abf3965f4e6d5537d523aa9f847ac05c971b3
JaySon-Huang <tshent@qq.com>
2021-09-12 16:13:24 +0800

    Fix
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- pkg/tidb/key_test.go
+++ pkg/tidb/key_test.go
@@ -11,7 +11,6 @@ func TestKey(t *testing.T) {
 	key, err := tidb.FromPDKey("7480000000000000FF375F72830000003DFF3FEC150000000000FA")
 	assert.Equal(t, err, nil)
 	assert.Equal(t, key.GetPDKey(), "7480000000000000FF375F72830000003DFF3FEC150000000000FA")
-	assert.True(t, false)
 	tableRow, err := key.GetTableRow()
 	assert.Equal(t, err, nil)
 	assert.Equal(t, int64(55), tableRow.TableID)
