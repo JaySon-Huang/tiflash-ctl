c37ce2f4db5d017581d24dcd143b3d3927df7c76
JaySon <tshent@qq.com>
2024-05-09 14:52:41 +0800

    Remove deprecated func (#24)
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- cmd/dispatch.go
+++ cmd/dispatch.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strings"
 
@@ -97,7 +97,7 @@ func curlTiFlash(ip string, httpPort int, query string) error {
 		return err
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
